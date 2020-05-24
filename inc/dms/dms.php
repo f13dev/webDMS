d59504f6fddc63cb426a83070145d18313b8850a
Jim Valentine <jv@f13dev.com>
2020-05-24 17:56:34 +0000

    Fixing footer

--- inc/dms/dms.php
+++ inc/dms/dms.php
@@ -27,7 +27,8 @@ if (isset($_GET['p'])) { $page = $_GET['p']; } else { $page = 'main'; }
 </div>
 
 <div id="page-bottom">
-
+  webDMS 0.4 beta &copy; 2020 <a href="https://f13dev.com">James Valentine</a><br>
+  1046 documents, using 554.76 MB, remaining 854.31 GB
 </div>
 
 
