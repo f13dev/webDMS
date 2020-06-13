9108cd922bd073b82dc6373ca4f9335b2c005708
Jim Valentine <jv@f13dev.com>
2020-06-13 22:03:49 +0000

    Adding comments to validate class

--- inc/class/validate.class.php
+++ inc/class/validate.class.php
@@ -12,35 +12,59 @@
         return preg_match($pattern, $input);
     }
 
+    /**
+     * Pure numeric
+     */
     public function numeric($input) {
         return preg_match("/[0-9]+/", $input);
     }
 
+    /**
+     * Check max length 
+     */
     public function length($input, $length=32) {
         return strlen($input) <= $length;
     }
 
+    /**
+     * Check the input has at least 1 upper case character
+     */
     public function hasUpper($input) {
         return preg_match("/[A-Z]/", $input);
     }
 
+    /**
+     * Check the inpute has at least 1 lower case character
+     */
     public function hasLower($input) {
         return preg_match("/[a-z]/", $input);
     }
 
+    /**
+     * Check the input has at least one special (non alpha numeric) character
+     */
     public function hasSpecial($input) {
         return preg_match("/\W/", $input);
     }
 
+    /**
+     * Validate the strength of a password
+     */
     public function password($input) {
         return $this->length($input, 7) && $this->hasUpper($input) && $this->hasLower($input) && $this->hasSpecial($input);
     }
 
+    /**
+     * Check a title string is acceptable
+     */
     public function title($input) {
         // alpha numeric, between 1 and 32 characters
         return $this->alphaNum($input) && !$this->length($input, 0) && $this->length($input);
     }
 
+    /**
+     * Check if a titile already exists
+    */
     public function titleExists($input) {
         global $dbc;
         $statement = $dbc->prepare("SELECT name FROM categories");
