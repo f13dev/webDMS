e33c23e8cf7780d79dd37cabf3640e41c06cddb2
Jim Valentine <jv@f13dev.com>
2020-06-13 23:12:50 +0000

    Adding comments to file.class

--- inc/class/file.class.php
+++ inc/class/file.class.php
@@ -36,48 +36,81 @@ Class document {
     }
   }
 
+  /**
+   * Returns true if a file is set
+   */
   public function isSet() {
     return $this->isSet;
   }
 
+  /**
+   * Returns the ID of the file
+   */
   public function getID() {
     return $this->ID;
   }
 
+  /**
+   * Returns the title of the file
+   */
   public function getTitle() {
     return $this->title;
   }
 
+  /**
+   * Returns the notes of the file
+   */
   public function getNotes() {
     return $this->notes;
   }
 
+  /**
+   * Returns the ID of the folder the file is assoicated with
+   */
   public function getFolder() {
     return $this->folder;
   }
 
+  /**
+   * Returns the title of the folder the file is associated with
+   */
   public function getFolderTitle() {
     $folder = new folder($this->getFolder());
     return $folder->getTitle();
   }
 
+  /**
+   * Returns the date the file was uploaded
+   */
   public function getUploadDate() {
     return date(DATE_FORMAT, strtotime($this->upload_date));
   }
 
+  /**
+   * Returns the date of the file
+   */
   public function getDocDate() {
     return date(DATE_FORMAT, strtotime($this->document_date));
   }
 
+  /**
+   * Returns the filename of the file
+   */
   public function getFile() {
     return $this->file;
   }
 
+  /**
+   * Returns the extension of the file
+   */
   public function getExtension() {
     $explode = explode('.', $this->getFile());
     return strtolower(end($explode));
   }
 
+  /**
+   * Returns the absolute URL of the file, this will become a temporary URL for the file
+   */
   public function getFileURL($asPDF = false) {
     $file = $this->getFile();
     if ($asPDF == true) {
@@ -86,10 +119,16 @@ Class document {
     return SITE_URL . SITE_DOCS . $file;
   }
 
+  /**
+   * Returns the psuedo name of the file
+   */
   public function getPsuedoName() {
     return str_replace(' ','_',$this->getTitle() . '.' . $this->getExtension());
   }
 
+  /**
+   * Returns the absolute OS path of the file
+   */
   public function getFileLocation($asPDF = false) {
     $file = $this->getFile();
     if ($asPDF == true) {
@@ -98,20 +137,32 @@ Class document {
     return SITE_ROOT . SITE_DOCS . $file;
   }
 
+  /**
+   * Returns the file name as a PDF
+   */
   private function getFileAsPDF() {
     return str_replace($this->getExtension(), 'pdf', $this->getFile());
   }
 
+  /**
+   * Returns the HTML value to embed the file as a PDF
+   */
   private function showPDF() {
     return '<object data="' . $this->getFileURL(true) . '#toolbar=0" type="application/pdf" width="100%" height="99%">
       alt : <a href="' . $this->getFileURL(true) . '">' . $this->getPsuedoName() . '</a>
     </object>';
   }
 
+  /**
+   * Returns the HTML value to embed the file as an image
+   */
   private function showImage() {
     return '<img src="' . $this->getFileURL() . '" class="imgPreview">';
   }
 
+  /**
+   * Returns the HTML value to embed the file as audio
+   */
   private function showAudio($type) {
     return '<h2>' . $this->getPsuedoName() . '</h2>
       <audio controls controlslist="nodownload">
@@ -120,6 +171,9 @@ Class document {
       </audio>';
   }
 
+  /**
+   * Returns the HTML value to embed an office file as a PDF
+   */
   private function showOfficeFile() {
     if (file_exists($this->getFileLocation(true)) || $this->generatePDF()) {
       return $this->showPDF();
@@ -128,6 +182,9 @@ Class document {
     }
   }
 
+  /**
+   * Generate a PDF file from an existing office file
+   */
   private function generatePDF() {
     $cmd = 'export HOME=/tmp && soffice --headless --convert-to pdf --outdir ' .  SITE_ROOT . SITE_DOCS . ' ' .  SITE_ROOT . SITE_DOCS . $this->getFile();
     exec($cmd);
@@ -136,14 +193,23 @@ Class document {
     return file_exists($this->getFileLocation(true));
   }
 
+  /**
+   * Returns a message for files which are not supported
+   */
   private function showFileNotSupported() {
     return '<h2>Error: Filetype not supported<h2>';
   }
 
+  /**
+   * Returns an error message for files which do not exist
+   */
   private function showFileNotExist() {
     return '<h2>Error: File could not be found</h2>';
   }
 
+  /**
+   * Returns HTML to embed a file, or an error message
+   */
   public function showFile() {
     if (file_exists($this->getFileLocation())) {
       $ext = $this->getExtension();
