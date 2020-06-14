0c4ca86eb01ae939469eca870bc1a1cda0e982b8
Jim Valentine <jv@f13dev.com>
2020-06-14 13:49:49 +0000

    Adding download to fileScraper

--- inc/dms/fileScraper.php
+++ inc/dms/fileScraper.php
@@ -1,15 +1,22 @@
 <?php
-require ('../cfg.php');
 // Check the user is logged in 
 session_start();
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     exit();
 } else {
+    // Requirements
+    require ('../cfg.php');
+    require ('../class/file.class.php');
     $file = SITE_DOCS . $_GET['file'];
     $filename = $_GET['file'];
 
     header('Content-type: '. mime_content_type($file));
-    header('Content-Disposition: inline; filename="'.$filename.'"');
+    if (isset($_GET['ID']) && isset($_GET['download'])) {
+        $doc = new document($_GET['ID']);
+        header('Content-Disposition: attachment; filename="'.$doc->getPsuedoName().'"');
+    } else {
+        header('Content-Disposition: inline; filename="'.$filename.'"');
+    }
     header('Content-Length:'.filesize($file));
     header('Accept-Ranges: bytes');
     @readfile($file);
