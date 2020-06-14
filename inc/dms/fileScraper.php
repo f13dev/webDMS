587667859957f24d7a39423ad4c955fe070d2afa
Jim Valentine <jv@f13dev.com>
2020-06-14 13:12:50 +0000

    Simplifying security

--- inc/dms/fileScraper.php
+++ inc/dms/fileScraper.php
@@ -1,34 +1,6 @@
 <?php
-// Include the settings
-require_once('../cfg.php');
-
-// Include the classes
-foreach (glob("../class/*.class.php") as $class) {
-  require $class;
-}
-
-// Include functions
-foreach (glob("../function/*.func.php") as $func) {
-  require $func;
-}
-
-// Initiate secure
-$security = new Secure();
-// Initiate validate
-$validate = new Validate();
-
-// Session data
+// Check the user is logged in 
 session_start();
-
-//$security->session_token();
-// Session security
-if (!isset($_SESSION['initiated'])) {
-  session_regenerate_id();
-  $_SESSION['initiated'] = true;
-}
-
-$security->checkFingerprint();
-
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     exit();
 } else {
