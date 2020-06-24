a36e79b455824821cb0d0f038ba030442a2c1b54
Jim Valentine <jv@f13dev.com>
2020-06-24 07:14:05 +0000

    More of email class;

--- inc/class/email.class.php
+++ inc/class/email.class.php
@@ -12,34 +12,56 @@ Class email {
     private $htmlBody;
     private $html;
 
+    /**
+     * Create a new instance of email 
+     */
     public function __construct() {
         $this->html = false;
     }
 
+    /**
+     * Set the recipient of the email
+     */
     public function setTo($to) {
         $this->to = $to;
     }
 
+    /**
+     * Set the sender of the email
+     */
     public function setFrom($from) {
         $this->from = $from;
     }
 
+    /**
+     * Set the email subject
+     */
     public function setSubject($subject) {
         $this->subject = $subject;
     }
 
+    /**
+     * Set the plain text body of the email
+     */
     public function setBody($body) {
         $this->body = $body;
     }
 
+    /**
+     * Set the html body of the email
+     */
     public function setHtmlBody($body) {
         $this->htmlBody = $body;
         $this->html = true;
     }
 
+    /**
+     * Send the email
+     */
     public function send() {
         if (MAIL_SERVER) {
             // Send via SMTP server
+            // need to include https://github.com/PHPMailer/PHPMailer
             $mail = new PHPMailer();
             $mail->IsSMTP();
             $mail->CharSet = 'UTF-8';
@@ -58,6 +80,8 @@ Class email {
             $mail->send();
         } else {
             // Send via PHP mail
+            $headers = "From: <" . $this->from . ">\r\n";
+            return mail($this->to,$this->subject,$this->body,$headers);
         }
     }
 }
